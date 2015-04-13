186cf1fbcc730f227658bd03c62e566eb2789800
zhangkangkang <zhangkang65@qq.com>
2015-04-13 17:30:12 +0800

    1

--- utreasure/src/main/java/com/zkk/utreasure/entity/ShUser.java
+++ utreasure/src/main/java/com/zkk/utreasure/entity/ShUser.java
@@ -1,5 +1,129 @@
 package com.zkk.utreasure.entity;
 
 public class ShUser {
+	
+	private  String shid;  //用户标识符
+	private  String  shName;//用户姓名
+	private  String shLoginName;//用户登录名
+	private  String shSex;//用户性别
+	private  String shAge;//用户年龄
+	private  String shPassword;//用户密码
+	private  String shCreateTime;//创建时间
+	private  String shUpdateTime;//更新时间
+	private  String shImage;//头像
+	private  String shAddr;//用户所在地
+	private  String shIdentityCard;//身份证
+	private  String shTelePhone;//电话
+	private  String shEmail;//用户邮箱
+	private  String shType;//用户类型
+	private  String shBat1;//
+	private  String shBat2;//
+	private  String shBat3;//
+	
+	
+	public String getShid() {
+		return shid;
+	}
+	public void setShid(String shid) {
+		this.shid = shid;
+	}
+	public String getShName() {
+		return shName;
+	}
+	public void setShName(String shName) {
+		this.shName = shName;
+	}
+	public String getShLoginName() {
+		return shLoginName;
+	}
+	public void setShLoginName(String shLoginName) {
+		this.shLoginName = shLoginName;
+	}
+	public String getShSex() {
+		return shSex;
+	}
+	public void setShSex(String shSex) {
+		this.shSex = shSex;
+	}
+	public String getShAge() {
+		return shAge;
+	}
+	public void setShAge(String shAge) {
+		this.shAge = shAge;
+	}
+	public String getShPassword() {
+		return shPassword;
+	}
+	public void setShPassword(String shPassword) {
+		this.shPassword = shPassword;
+	}
+	public String getShCreateTime() {
+		return shCreateTime;
+	}
+	public void setShCreateTime(String shCreateTime) {
+		this.shCreateTime = shCreateTime;
+	}
+	public String getShUpdateTime() {
+		return shUpdateTime;
+	}
+	public void setShUpdateTime(String shUpdateTime) {
+		this.shUpdateTime = shUpdateTime;
+	}
+	public String getShImage() {
+		return shImage;
+	}
+	public void setShImage(String shImage) {
+		this.shImage = shImage;
+	}
+	public String getShAddr() {
+		return shAddr;
+	}
+	public void setShAddr(String shAddr) {
+		this.shAddr = shAddr;
+	}
+	public String getShIdentityCard() {
+		return shIdentityCard;
+	}
+	public void setShIdentityCard(String shIdentityCard) {
+		this.shIdentityCard = shIdentityCard;
+	}
+	public String getShTelePhone() {
+		return shTelePhone;
+	}
+	public void setShTelePhone(String shTelePhone) {
+		this.shTelePhone = shTelePhone;
+	}
+	public String getShEmail() {
+		return shEmail;
+	}
+	public void setShEmail(String shEmail) {
+		this.shEmail = shEmail;
+	}
+	public String getShType() {
+		return shType;
+	}
+	public void setShType(String shType) {
+		this.shType = shType;
+	}
+	public String getShBat1() {
+		return shBat1;
+	}
+	public void setShBat1(String shBat1) {
+		this.shBat1 = shBat1;
+	}
+	public String getShBat2() {
+		return shBat2;
+	}
+	public void setShBat2(String shBat2) {
+		this.shBat2 = shBat2;
+	}
+	public String getShBat3() {
+		return shBat3;
+	}
+	public void setShBat3(String shBat3) {
+		this.shBat3 = shBat3;
+	}
+	
+	
 
 }
